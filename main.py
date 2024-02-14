376e0e6bb35f3a5415b1e3d06e20b33b0da004ae
kalivhack <96864261+kalivhack@users.noreply.github.com>
2024-02-14 17:43:10 +0000

    Some Annoying stuff changed

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
+# import threading
 import requests
 import json
 import os
 
+from time import sleep
 from pytimedinput import timedInput
 from getpass import getuser
 
+from colorama import Fore
 from fake_useragent import UserAgent
 from email_validator import validate_email 
-
 
 # Code
 def load_json(file: str) -> None:
@@ -28,26 +30,26 @@ def dump_2_json(data: dict | str, file: str) -> None:
         
         os.chdir(folder)
     
-    with open(file[-1], 'xt') as json_file:
+    with open(file[-1], 'w+t') as json_file:
         json.dump(data, json_file, indent=4)
 
 
-def temp_mail(email: str, api: str) -> None:
+def temp_mail(email: str, api: str) -> bool:
     email = email.split('@')
 
     # Fetching single message: https://www.1secmail.com/api/v1/?action=readMessage&login=demo&domain=1secmail.com&id=639
-    print(f'Hey {getuser()}. Waiting for messages\n')
+    print(f'{Fore.LIGHTYELLOW_EX}[INFO] Waiting for messages\n')
 
     while True:
         ua = UserAgent().random
         messages = requests.get(api + f'getMessages&login={email[0]}&domain={email[1]}', json={'user-agent': ua}).json()
 
         if messages:
-            print(f'Geted {len(messages)} New Messages\n')
+            print(f'{Fore.LIGHTCYAN_EX}[INFO] Geted {len(messages)} New Messages\n')
             data = load_json(f'files/json/{getuser()}.json')
 
             for index, message in enumerate(messages):
-                if input(f'{index+1}. Message from {message["from"]}. Show ? '):
+                if input(f'{Fore.LIGHTGREEN_EX}[+] {index+1}. Message from {message["from"]}. Show ? '):
                     full_message = requests.get(api + f'readMessage&login={email[0]}&domain={email[1]}&id={message["id"]}', json={'user-agent': ua}).json()
                     
                     # writing to json file
@@ -59,30 +61,34 @@ def temp_mail(email: str, api: str) -> None:
                         data[message["from"]].append(full_message)
 
                     for name, value in full_message.items():
-                        print(f'   {name}: {value}')
+                        print(f'   {Fore.LIGHTYELLOW_EX}{name}: {Fore.LIGHTGREEN_EX}{value}')
             
             # Dumping data to json  file
             dump_2_json(data, f'files/json/{getuser()}.json')
 
-        key, successful = timedInput(f'Press \'q\' to exit: ', timeout=3)
-
-        if not successful and key == 'q':
-            exit()
+        sleep(3)
+    
+    return True
 
         
-
-    
 if __name__ == '__main__':
-    print(f'{" Temp-Mail ":=^65}\n')
+    print(f'{Fore.LIGHTBLACK_EX}{" Temp-Mail ":=^65}\n')
     
     API: str = 'https://www.1secmail.com/api/v1/?action='
     EMAIL: str = requests.get(API + 'genRandomMailbox', ).json()[0]
     
-    print(f'Your Temp Email: {EMAIL}')
+    print(f'{Fore.LIGHTYELLOW_EX}Hey {Fore.LIGHTMAGENTA_EX + getuser()}. {Fore.LIGHTYELLOW_EX}Press \'Ctrl + C\' to exit')
+    print(f'{Fore.LIGHTGREEN_EX}[INFO] Your Temp Email: {EMAIL}')
     
     try:
         validate_email(EMAIL)
         temp_mail(email=EMAIL, api=API)
     
+    except KeyboardInterrupt:
+        print(f'{Fore.LIGHTRED_EX}[STOPPED] Ctrl+C detected exiting!')
+
     except BaseException as ex:
-        print(ex)       
+        print(f'{Fore.LIGHTRED_EX}[ERROR] {ex}')
+
+    finally:
+        print(Fore.WHITE)
